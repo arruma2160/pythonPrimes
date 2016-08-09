7cdfe1006b1992c6dc60c814294a38f19db1acef
Arturo Ruiz Mañas <arruma2160@gmail.com>
2016-08-09 23:01:30 +0100

    Simple test

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 
 from Erastosthenes import Erastosthenes
+from Sundaram import Sundaram
 
-erastosthenes = Erastosthenes(10000)
+erastosthenes = Erastosthenes(100000)
 
 for prime in erastosthenes:
     print (prime, end=" ")
@@ -10,3 +11,14 @@ for prime in erastosthenes:
 print()
 
 print ("erastosthenes.time_exec = " + str( erastosthenes.execution_time()) )
+
+
+print ( " ==================== " )
+
+sundaram = Sundaram(100000)
+for prime in sundaram:
+    print ( prime, end = " " )
+
+print()
+
+print ("sundaram.time_exec = " + str( sundaram.execution_time() ) )
